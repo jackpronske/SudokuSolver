0a57ac67fea2e8e835fe7c7cdba46ea6d7cc68f4
Jack <jopronske@gmail.com>
2021-08-10 07:41:54 -0500

    9x9 solver is completed, needs to be optimized but can solve easy and medium puzzles

--- solver.test.js
+++ solver.test.js
@@ -288,43 +288,58 @@ describe("Completed Board Tests", () => {
   });
 });
 
-// describe("Generating Complete Boards", () => {
-//   test("returns the input board if it is already valid", () => {
-//     const board = new Board([
-//       [1, 2, 3, 4],
-//       [3, 4, 1, 2],
-//       [2, 1, 4, 3],
-//       [4, 3, 2, 1],
-//     ]);
+describe("Generating Complete Boards", () => {
+  test("returns the input board if it is already valid", () => {
+    const board = new Board([
+      [5, 3, 4, 6, 7, 8, 9, 1, 2],
+      [6, 7, 2, 1, 9, 5, 3, 4, 8],
+      [1, 9, 8, 3, 4, 2, 5, 6, 7],
+      [8, 5, 9, 7, 6, 1, 4, 2, 3],
+      [4, 2, 6, 8, 5, 3, 7, 9, 1],
+      [7, 1, 3, 9, 2, 4, 8, 5, 6],
+      [9, 6, 1, 5, 3, 7, 2, 8, 4],
+      [2, 8, 7, 4, 1, 9, 6, 3, 5],
+      [3, 4, 5, 2, 8, 6, 1, 7, 9],
+    ]);
 
-//     expect(solver(board)).toBeInstanceOf(Array);
-//   });
+    expect(solver(board)).toBeInstanceOf(Array);
+  });
 
-//   test("returns error if board is impossible to solve", () => {
-//     const board = new Board([
-//       [1, 0, 0, 0],
-//       [0, 0, 0, 0],
-//       [0, 0, 0, 0],
-//       [1, 0, 0, 0],
-//     ]);
+  test("returns error if board is impossible to solve", () => {
+    const board = new Board([
+      [5, 3, 4, 6, 7, 0, 0, 0, 5],
+      [6, 7, 2, 1, 9, 5, 3, 4, 8],
+      [1, 9, 8, 3, 4, 2, 5, 6, 7],
+      [8, 5, 9, 7, 6, 1, 4, 2, 3],
+      [4, 2, 6, 8, 5, 3, 7, 9, 1],
+      [7, 1, 3, 9, 2, 4, 8, 5, 6],
+      [9, 6, 1, 5, 3, 7, 2, 8, 4],
+      [2, 8, 7, 4, 1, 9, 6, 3, 5],
+      [3, 4, 5, 2, 8, 6, 1, 7, 9],
+    ]);
 
-//     expect(solver(board)).toBeInstanceOf(Array);
-//   });
+    expect(solver(board)).toBeInstanceOf(Array);
+  });
 
-//   test("generates a valid board from scratch", () => {
-//     expect(solver()).toBeInstanceOf(Array);
-//   });
+  // test("generates a valid board from scratch", () => {
+  //   expect(solver()).toBeInstanceOf(Array);
+  // });
 
-//   test("generates a valid board from a partially filled in board", () => {
-//     expect(
-//       solver(
-//         new Board([
-//           [3, 0, 0, 0],
-//           [0, 0, 2, 0],
-//           [0, 1, 0, 0],
-//           [0, 0, 0, 1],
-//         ])
-//       )
-//     ).toBeInstanceOf(Array);
-//   });
-// });
+  test("generates a valid board from a partially filled in board", () => {
+    expect(
+      solver(
+        new Board([
+          [6, 0, 0, 8, 0, 0, 3, 0, 0],
+          [5, 0, 0, 0, 2, 0, 0, 0, 0],
+          [1, 0, 0, 4, 0, 3, 0, 5, 6],
+          [0, 6, 1, 0, 0, 0, 5, 9, 8],
+          [9, 0, 4, 6, 0, 5, 0, 0, 2],
+          [8, 3, 5, 0, 0, 9, 7, 0, 0],
+          [3, 0, 9, 5, 0, 8, 4, 0, 0],
+          [7, 8, 0, 0, 4, 2, 9, 1, 0],
+          [0, 0, 2, 0, 0, 0, 0, 8, 0],
+        ])
+      )
+    ).toBeInstanceOf(Array);
+  });
+});
